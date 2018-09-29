94344ab70ad9fbd27f6456b12289d738ada59929
prajeshsharma <42785780+prajeshsharma@users.noreply.github.com>
2018-09-28 21:37:24 -0400

    I debugged.

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -3,11 +3,10 @@ public class MathUtils {
      * divideNumbers -- a / b
      * Divide a by b and return the result
      */
-    public static double divideNumbers(int dividend, int divisor) {
+    public static double divideNumbers(double dividend, int divisor) {
         /* Do some casual division ... Super easy ... Shouldn't be any problems here */
-        double res = dividend / divisor;
         /* Return the DEFINITELY CORRECT result */
-        return res;
+        return dividend / divisor;
     }
 
     /**
@@ -16,7 +15,9 @@ public class MathUtils {
      */
     public static double raiseToPower(int base, int exp) {
         /* Set initial result to be just the number */
-        int res = base;
+        double res = 1;
+
+
 
         /* Determine if the exponent is negative */
         boolean isNegative = false;
@@ -34,8 +35,7 @@ public class MathUtils {
 
         /* If it's a negative exponent, we should invert it! */
         if (isNegative) {
-            double dividedRes = divideNumbers(1, res);
-            return dividedRes;
+            return 1 / res;
         }
         /* Otherwise, we are safe to just return the result */
         else {
